920ef1d4171b78ca0ef40839e386f27aecab04a8
Hamid Purhasanih <purhasanih@gmail.com>
2023-04-09 10:48:40 +0330

    split car

---
+++ fragmentor.py
@@ -0,0 +1,35 @@
+# import pandas as pd
+
+# df = pd.read_excel('Cars.xlsx', sheet_name='Sheet1')
+
+# new_df = pd.DataFrame(columns=['Code', 'PersianName', 'CarModel'])
+
+# for i, row in df.iterrows():
+#     carModels = row['CarModels'].split(',')
+    
+#     for carModel in carModels:
+#         new_df = new_df.append({'Code': row['Code'], 'PersianName': row['PersianName'], 'CarModel': carModel}, ignore_index=True)
+
+# new_df.to_excel('new_cars.xlsx', index=False)
+
+
+import pandas as pd
+
+df = pd.read_excel('Cars.xlsx')
+
+new_rows = []
+for _, row in df.iterrows():
+    code = row['Code']
+    persian_name = row['PersianName']
+    if type(row['CarModels']) == str:
+        carModels = row['CarModels'].split(',')
+        print(carModels)
+        for car_model in carModels:
+            new_rows.append({'Code': code, 'PersianName': persian_name, 'CarModel': car_model})
+    else:
+        new_rows.append({'Code': code, 'PersianName': persian_name, 'CarModel': ''})
+
+new_df = pd.DataFrame(new_rows)
+
+# Save the new DataFrame to a new Excel file
+new_df.to_excel('new_cars.xlsx', index=False)
